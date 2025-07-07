108ae913d50d2f26be4911bb73cd0c7a384c7d7c
w.bieniek <82888679+Waldzio@users.noreply.github.com>
2025-07-08 00:05:04 +0200

    Update run.sh

--- ping_logger/run.sh
+++ ping_logger/run.sh
@@ -1,6 +1,12 @@
-#!/usr/bin/with-contenv bashio
-export SUPERVISOR_MQTT_USERNAME=$(bashio::services "mqtt" "username")
-export SUPERVISOR_MQTT_PASSWORD=$(bashio::services "mqtt" "password")
-export SUPERVISOR_MQTT_HOST=$(bashio::services "mqtt" "host")
-export SUPERVISOR_MQTT_PORT=$(bashio::services "mqtt" "port")
+#!/usr/bin/env bash
+
+# Jeśli Supervisor (bashio) jest dostępny, pobierz poświadczenia:
+if command -v bashio &> /dev/null; then
+  export SUPERVISOR_MQTT_USERNAME=$(bashio::services mqtt "username")
+  export SUPERVISOR_MQTT_PASSWORD=$(bashio::services mqtt "password")
+  export SUPERVISOR_MQTT_HOST=$(bashio::services mqtt "host")
+  export SUPERVISOR_MQTT_PORT=$(bashio::services mqtt "port")
+fi
+
+# Uruchom PingLogger
 python3 /ping_logger.py
