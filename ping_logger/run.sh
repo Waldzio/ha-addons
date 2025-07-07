ece46091404ebe30dffa539b37330ca9ef418489
w.bieniek <82888679+Waldzio@users.noreply.github.com>
2025-07-08 01:58:15 +0200

    Update run.sh

--- ping_logger/run.sh
+++ ping_logger/run.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/env bash
 
-# Teraz Bashio jest dostępne w oficjalnych obrazach
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_PORT=$(bashio::services mqtt "port")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASS=$(bashio::services mqtt "password")
+# Jeśli jesteśmy pod Supervisor, pobierz poświadczenia MQTT
+if command -v bashio &> /dev/null; then
+  export MQTT_HOST=$(bashio::services mqtt "host")
+  export MQTT_PORT=$(bashio::services mqtt "port")
+  export MQTT_USER=$(bashio::services mqtt "username")
+  export MQTT_PASS=$(bashio::services mqtt "password")
+fi
 
-export MQTT_HOST MQTT_PORT MQTT_USER MQTT_PASS
-
+# Uruchom główny skrypt
 python3 /ping_logger.py
