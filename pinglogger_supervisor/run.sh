d94f5c11d8ec886768affbe728a6fd7406120d51
w.bieniek <82888679+Waldzio@users.noreply.github.com>
2025-07-07 23:03:18 +0200

    Delete pinglogger_supervisor directory

--- pinglogger_supervisor/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-export SUPERVISOR_MQTT_USERNAME=$(bashio::services "mqtt" "username")
-export SUPERVISOR_MQTT_PASSWORD=$(bashio::services "mqtt" "password")
-export SUPERVISOR_MQTT_HOST=$(bashio::services "mqtt" "host")
-export SUPERVISOR_MQTT_PORT=$(bashio::services "mqtt" "port")
-python3 /ping_logger.py
