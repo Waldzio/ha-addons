402844e747c8a8bac96f881ddc904a8871412682
w.bieniek <82888679+Waldzio@users.noreply.github.com>
2025-07-08 02:12:48 +0200

    Update run.sh

--- ping_logger/run.sh
+++ ping_logger/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Jeśli jesteśmy pod Supervisor, pobierz poświadczenia MQTT
+# Jeśli bashio jest dostępne (Supervisor Add-on), pobierz poświadczenia MQTT
 if command -v bashio &> /dev/null; then
   export MQTT_HOST=$(bashio::services mqtt "host")
   export MQTT_PORT=$(bashio::services mqtt "port")
