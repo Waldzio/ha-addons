46330a9ade7fe959c68e04cb2efabe6a636bc7f7
w.bieniek <82888679+Waldzio@users.noreply.github.com>
2025-07-08 05:18:02 +0200

    Add files via upload

---
+++ ping_logger_0_7_0/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if command -v bashio &> /dev/null; then
+  export MQTT_HOST=$(bashio::services mqtt "host")
+  export MQTT_PORT=$(bashio::services mqtt "port")
+  export MQTT_USER=$(bashio::services mqtt "username")
+  export MQTT_PASS=$(bashio::services mqtt "password")
+fi
+
+python3 /ping_logger.py
